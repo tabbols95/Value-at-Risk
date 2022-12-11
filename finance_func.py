0805e0b9339e6c6f338ca97024a17f4ccd08b62d
Alexander Shilov <tabbols95@gmail.com>
2022-12-12 01:10:21 +0300

    create function value at risk (var)

--- finance_func.py
+++ finance_func.py
@@ -15,3 +15,21 @@ def volatility(returns, periods=1):
     if isinstance(returns, pd.DataFrame):
         return returns.pct_change(periods=periods).std(axis=0).mul(100)
 
+def value_at_risk(returns, confidence_level=.05):
+    """
+    Return Value at Risk.
+    
+    Parameters
+    ----------
+    returns : pd.DataFrame
+        Description
+    confidence_level : float, default 0.05
+        Description
+    
+    Returns
+    -------
+    var : pd.Series
+    """
+    
+    # Calculating Value at Risk
+    return returns.quantile(confidence_level)
