53d07ae016d18fef6859d08492952a9dc3d9eeb3
Alexander Shilov <tabbols95@gmail.com>
2022-12-12 17:24:31 +0300

    Creare functions change(data, periods=1)

--- finance_func.py
+++ finance_func.py
@@ -1,5 +1,67 @@
 import pandas as pd
 
+def change(data, periods=1):
+    """
+    Percentage change between the current and a prior element.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame or pandas.Series
+        Initial data.
+    periods : int, default 1
+        Periods to shift for forming percent change.
+    
+    Returns
+    -------
+    chg : pandas.DataFrame or pandas.Series
+        The same type as the calling object.
+    
+    Examples
+    --------
+    **Series**
+    
+    >>> s = pd.Series([20, 25, 27.2], name='TST')
+    >>> s
+    0    20.0
+    1    25.0
+    2    27.2
+    Name: TST, dtype: float64
+    
+    >>> ff.change(s)
+    0      NaN
+    1    0.250
+    2    0.088
+    Name: TST, dtype: float64
+    
+    **DataFrame**
+    
+    >>> df = pd.DataFrame({
+    ...    'TST0': [1.365, 1.352, 1.587],
+    ...    'TST1': [865.39, 901.12, 899.02],
+    ...    'TST2': [12.83, np.nan, 12.95]},
+    ...    index=['2022-01-01', '2022-01-02', '2022-01-03'])
+    >>> df
+                 TST0    TST1   TST2
+    2022-01-01  1.365  865.39  12.83
+    2022-01-02  1.352  901.12    NaN
+    2022-01-03  1.587  899.02  12.95
+    >>> ff.change(df)
+                     TST0       TST1      TST2
+    2022-01-01        NaN        NaN       NaN
+    2022-01-02  -0.009524   0.041288  0.000000
+    2022-01-03   0.173817  -0.002330  0.009353
+    
+    **Other type data**
+    
+    >>> ff.change(55)
+    'MsgError: data type pandas.DataFrame or pandas.Series'
+    """
+    
+    if isinstance(data, pd.DataFrame) or isinstance(data, pd.Series):
+        return data.pct_change(periods=periods, fill_method='ffill')
+    else: return ('MsgError: data type pandas.DataFrame or pandas.Series')
+        
+
 def volatility(returns, periods=1):
     """
     Parameters
