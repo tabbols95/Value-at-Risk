209827aea705d148ef12159263d29b9e146bd76a
Alexander Shilov <tabbols95@gmail.com>
2022-12-15 16:32:20 +0300

    create vlt function

--- finance_func.py
+++ finance_func.py
@@ -132,22 +132,81 @@ def change_ln(data, periods=1):
     data = data.fillna(method='pad')
     return np.log(data/data.shift(periods))
 
-def volatility(returns, periods=1):
+def vlt(data, periods=1, axis=0, flag_change=True):
     """
+    Getting daily, monthly percentage returns from ticker close prices.
+    
     Parameters
     ----------
-    returns : pandas.DataFrame
+    data : pandas.DataFrame or pandas.Series
+        Initial data.
+    periods : int, default 1
+        Periods to shift for forming percent change.
+    axis : int, default 0
+        For pandas.Series this parameter is unused and defaults to 0.
+    flag_change : bool, default True
+        If True, calculate change functions otherwise calculate change_ln.
+        
     
     Returns
     -------
-    volatility : pd.Series
+    vlt : float or pandas.Series
+        The same type as the calling object.
+    
+    Examples
+    --------
+    **Series**
+    
+    >>> s = pd.Series([12.36, 12.25, np.nan, np.nan, 11.85, 11.93])
+    >>> s
+    0    12.36
+    1    12.25
+    2      NaN
+    3      NaN
+    4    11.85
+    5    11.93
+    dtype: float64
+    
+    >>> ff.vlt(s)
+    0.01540107110011917
+    
+    >>> ff.vlt(s, flag_change=False)
+    0.015624781360881121
+    
+    **DataFrame**
+    
+    >>> df = pd.DataFrame({
+    ...    'TST1': [85, 86, 29],
+    ...    'TST2': [36.2, 37.8, 39.9],
+    ...    'TST3': [0.1685, np.nan, 0.1683]})
+    >>> df
+       TST1  TST2    TST3
+    0    85  36.2  0.1685
+    1    86  37.8     NaN
+    2    29  39.9  0.1683
+    
+    >>> ff.vlt(df)
+    TST1    0.476983
+    TST2    0.008030
+    TST3    0.000839
+    dtype: float64
+    
+    >>> ff.vlt(df, flag_change=False)
+    TST1    0.776932
+    TST2    0.007649
+    TST3    0.000840
+    dtype: float64
     """
     
     # Calculating volatility
-    if isinstance(returns, pd.DataFrame):
-        return returns.pct_change(periods=periods).std(axis=0).mul(100)
+    if flag_change:
+        vlt = change(data, periods=1).std(axis=axis)
+    elif not flag_change:
+        vlt = change_ln(data, periods=1).std(axis=axis)
+        
+    return vlt
 
-def value_at_risk(returns, confidence_level=.05):
+def value_at_risk(returns, confidence_level=.05, periods=1, axis=0, flag_change=True):
     """
     Return Value at Risk.
     
